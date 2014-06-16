93ce07880d2ef02b4de82f475b2791593251482e
mattintosh4 <mattintosh4@gmail.com>
2014-06-17 02:13:58 +0900

    .

--- osx109dmggen.sh
+++ osx109dmggen.sh
@@ -2,32 +2,14 @@
 # 
 # Easy script for creating OS X Mavericks installation disk image
 # 
+# Copyright (c) 2014 mattintosh4
+# Released under the MIT license
+# https://github.com/mattintosh4/osx109dmggen/blob/master/LICENSE
+# 
 # Operation confirmation:
 #    OS X 10.6.8
 #    OS X 10.9.3
 # 
-# The MIT License (MIT)
-# 
-# Copyright (c) 2014 mattintosh4
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
 
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
