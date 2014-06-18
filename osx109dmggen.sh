a14960e41dce2cf4a555fefb28ce1b14fb8b435a
mattintosh4 <mattintosh4@gmail.com>
2014-06-19 00:33:23 +0900

    .

--- osx109dmggen.sh
+++ osx109dmggen.sh
@@ -12,28 +12,20 @@
 # 
 
 set -e
-
-PATH=/usr/bin:/bin:/usr/sbin:/sbin
+#set -x
+PATH=/usr/bin:/bin
 
 InstallESD_dmg="/Applications/Install OS X Mavericks.app/Contents/SharedSupport/InstallESD.dmg"
+test -f "${InstallESD_dmg}" || { echo "'${InstallESD_dmg}' is not found."; exit 1; }
 
-if test ! -f "${InstallESD_dmg}"
-then
-    echo "${InstallESD_dmg} is not found."
-    exit 1
-fi
-
-temp=/tmp/`uuidgen`.sparseimage
-
-set -x
-
+sparseimage=/tmp/`uuidgen`.sparseimage
 hdiutil attach "${InstallESD_dmg}" -nobrowse -mountpoint /Volumes/ESD
-hdiutil convert /Volumes/ESD/BaseSystem.dmg -ov -format UDSP -o ${temp}
-hdiutil resize ${temp} -size 6g
-hdiutil attach ${temp} -nobrowse -mountpoint /Volumes/BSD
+hdiutil convert /Volumes/ESD/BaseSystem.dmg -ov -format UDSP -o ${sparseimage}
+hdiutil resize ${sparseimage} -size 6g
+hdiutil attach ${sparseimage} -nobrowse -mountpoint /Volumes/BSD
 rm /Volumes/BSD/System/Installation/Packages
-ditto /Volumes/ESD/Packages /Volumes/BSD/System/Installation/Packages
+ditto -V /Volumes/ESD/Packages /Volumes/BSD/System/Installation/Packages
 hdiutil detach /Volumes/BSD
 hdiutil detach /Volumes/ESD
-hdiutil convert ${temp} -ov -format UDRO -o "Install OS X Mavericks"
-rm ${temp}
+hdiutil convert ${sparseimage} -ov -format UDRO -o "Install OS X Mavericks"
+rm ${sparseimage}
